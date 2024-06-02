766aecd5bb7fcfbdac5f35d9e35cf75e4f089edb
abdkm-nr <166373061+abdkm-nr@users.noreply.github.com>
2024-06-02 15:36:48 +0500

    tireddd

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
-import Services from './components/Services';
 import CatalogPage from "./components/CatalogPage";
 import MoviePage from "./components/MoviePage";
 import { moviedata } from './components/moviedata';
@@ -17,7 +16,6 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services services={['Web Development', 'App Development', 'SEO Optimization']} />} />
           <Route path="/CatalogPage" element={<CatalogPage />} />
           <Route exact path="/movies" element={<CatalogPage />} />
           <Route path="/movies/:id" element={<MoviePage movies={moviedata} />} />
